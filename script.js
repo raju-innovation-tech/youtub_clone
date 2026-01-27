04466c2ee6a6aa981f9154d4261b9732d627d62f
Raju Kumar Maurya <rkmaury860@gmail.com>
2026-01-27 15:58:35 +0530

    sidebar completed

---
+++ script.js
@@ -0,0 +1,7 @@
+let menuIcon = document.body.querySelector(".menu-icon");
+let sidebar = document.body.querySelector(".sidebar");
+
+menuIcon.onclick=function(){
+    sidebar.classList.toggle("small-sidebar");
+
+}
