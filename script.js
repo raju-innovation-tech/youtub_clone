300ef473df1bfce1c9a0c8de0489aa9934a779e1
raju-innovation-tech <rkmaury860@gmail.com>
2026-01-27 21:50:47 +0530

    forntend first page complete

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let menuIcon = document.body.querySelector(".menu-icon");
 let sidebar = document.body.querySelector(".sidebar");
+let container = document.body.querySelector(".container");
 
 menuIcon.onclick=function(){
     sidebar.classList.toggle("small-sidebar");
-
+    container.classList.toggle("large-container") ;
 }
